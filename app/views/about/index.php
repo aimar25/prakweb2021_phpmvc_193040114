4a09bad7db0592633846ceef6eeffba1acc4924b
aimar25 <aimarsak@gmail.com>
2021-10-01 19:49:25 +0700

    sudah

---
+++ app/views/about/index.php
@@ -0,0 +1,9 @@
+<div class="container">
+
+        <h1 class ="mt-4">Index ni boss</h1>
+        <img src="<?= BASEURL; ?>/img/1.jpg" alt="Aimar" widht="200"
+        class="rounded-circle shadow">
+        <p>Halo, saya <?= $data['nama']; ?>, umur <?= $data['umur']; ?>  tahun, saya seorang
+         <?= $data['pekerjaan']; ?> </p>
+
+</div>
---
+++ app/views/about/page.php
@@ -0,0 +1,3 @@
+
+        <h1>Page saya</h1>
+  
